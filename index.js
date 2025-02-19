898571f5ec56f0588a63c1cb17bcc0f71b918b6b
Jackson Klingaman <jackson.klingaman22@gmail.com>
2025-02-19 16:32:10 -0600

    passes all tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const name = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards(name, event) {
+    let thankYou = []
+    for (let i = 0; i < name.length; i++) {
+      thankYou.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        
+    }
+    return thankYou;
+  }
+
+function countDown() {
+    let subtract = 10;
+    while (subtract > -1) {
+  console.log(subtract--);
+}}
